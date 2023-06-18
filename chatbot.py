b95c3a1ae3f10cfe081209fcd1b2d399e927c1d3
Pranshumaan <pranshumaan@pop-os.localdomain>
2023-06-18 09:53:26 +0530

    Restructured folders, updated requirements.txt

--- chatbot.py
+++ chatbot.py
@@ -7,6 +7,8 @@ from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 
+import pickle
+
 from typing import List, Tuple
 
 from nicegui import Client, ui
@@ -17,32 +19,9 @@ with open('../Secrets/open_ai_api_key.txt', 'r') as file:
 
 os.environ['OPENAI_API_KEY'] = key
 
-#Reading in all PDFs
-folder_path = "/media/pranshumaan/TOSHIBA EXT/Dev/Project_Finmin/test_folder/"
-loader = PyPDFDirectoryLoader(folder_path)
-docs = loader.load()
-
-# read data from the file and put them into a variable called raw_text
-raw_text = ''
-for doc in docs:
-    text = doc.page_content
-    if text:
-        raw_text += text
-
-# Splitting up the text into smaller chunks for indexing
-text_splitter = CharacterTextSplitter(        
-    separator = "\n",
-    chunk_size = 1000,
-    chunk_overlap  = 200, #striding over the text
-    length_function = len,
-)
-texts = text_splitter.split_text(raw_text)
-
-#Making the embeddings
-embeddings = OpenAIEmbeddings()
-
-docsearch = FAISS.from_texts(texts, embeddings)
-# This is the vector store
+# Loading embeddings
+with open("/media/pranshumaan/TOSHIBA EXT/Dev/Project_Finmin/vectorstore.pkl", "rb") as f:
+    docsearch = pickle.load(f)
 
 chain = load_qa_chain(OpenAI(), chain_type="stuff")
 
